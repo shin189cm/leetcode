dc47b467ab37e92dde013c1fbab23c76aa28888b
shin189cm <156651244+shin189cm@users.noreply.github.com>
2026-08-24 13:42:33 +0900

    何を解いて、どんな計算量で実装したか、今後は記載する
    例: feat(stack): solve 0155 min-stack (Time: O(1), Space: O(N))

---
+++ 0014_Longest_Common_Prefix.py
@@ -0,0 +1,20 @@
+"""
+Problem: 
+URL: https://leetcode.com/problems/longest-common-prefix/
+Difficulty: Easy
+Category: XXX
+
+Complexity:
+- Time: O() - XX
+- Space: O() - XX
+
+Approach:
+1. XX
+2. XX
+3. XX
+"""
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        while:
+            for i in range(len(strs)):
+                strs[i,]
