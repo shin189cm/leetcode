b2e691fb6e17f2e7d643e4af644038c29ba85787
shin189cm <156651244+shin189cm@users.noreply.github.com>
2026-08-24 13:23:32 +0900

    何を解いて、どんな計算量で実装したか」本当は書きたい
    例: feat(stack): solve 0155 min-stack (Time: O(1), Space: O(N))

---
+++ 01_two_pointers/0167_two_sum_ii.py
@@ -0,0 +1,21 @@
+"""
+Problem: 0167_two_sum_ii.py
+URL: https://leetcode.com/problems/two-sum/
+Difficulty: Easy
+Category: XX
+
+Complexity:
+- Time: O() - XX
+- Space: O() -XX
+
+Approach:
+1. XX
+2. XX
+3. XX
+"""
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # 要素を保存する場所
+        diff = {}
+        for i in range(len_list):
+            if target - nums[i]
