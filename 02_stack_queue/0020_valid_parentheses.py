8bb97732895ab2cc4331a05e463897f076fd89e8
shin189cm <156651244+shin189cm@users.noreply.github.com>
2026-08-24 13:26:54 +0900

    何を解いて、どんな計算量で実装したか、今後は記載する
    例: feat(stack): solve 0155 min-stack (Time: O(1), Space: O(N))

---
+++ 02_stack_queue/0020_valid_parentheses.py
@@ -0,0 +1,18 @@
+"""
+Problem: 
+URL: https://leetcode.com/problems/valid-parentheses/description/
+Difficulty: Easy
+Category: XXX
+
+Complexity:
+- Time: O() - XX
+- Space: O() - XX
+
+Approach:
+1. XX
+2. XX
+3. XX
+"""
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
