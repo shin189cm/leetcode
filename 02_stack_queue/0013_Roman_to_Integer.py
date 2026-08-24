7728e680c24e84a355a1ddb87a2cfcb93ddf717a
shin189cm <156651244+shin189cm@users.noreply.github.com>
2026-08-24 13:29:23 +0900

    何を解いて、どんな計算量で実装したか、今後は記載する
    例: feat(stack): solve 0155 min-stack (Time: O(1), Space: O(N))

---
+++ 02_stack_queue/0013_Roman_to_Integer.py
@@ -0,0 +1,24 @@
+"""
+Problem: 
+URL: 
+Difficulty: Easy
+Category: XXX
+
+Complexity:
+- Time: O() - XX
+- Space: O() - XX
+
+Approach:
+1. XX
+2. XX
+3. XX
+"""
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        values = {
+            'I': 1,'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+        }
+
+        total = 0
+        n =
+        for i in range(len(str)):
