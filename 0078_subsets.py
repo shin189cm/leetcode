524cabd11761a72bef5f5ad5a2fc60c8e54fd00c
shin189cm <156651244+shin189cm@users.noreply.github.com>
2026-08-30 21:31:47 +0900

    初見で解いてみた。方針しか書けず。

---
+++ 0078_subsets.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        """
+        部分集合を返す問題。
+        空集合も含める。
+        ダブりはNG。
+        順番は自由。
+        ずるいが、これはDFS（Depth First Search, Breadth First Searchの略）。
+        再帰的に求める。
+        含む、含まないの2分岐のツリーを作成するイメージ。
+        """
+        if len(nums) == 0:
+            return []
+        def yesno(self, subset: List[int]):
+
+        for i in range(len(nums)):
+            yesno()
