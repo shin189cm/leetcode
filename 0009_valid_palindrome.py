01de45afee05a584b452893282c6418f679ccafa
shin189cm <156651244+shin189cm@users.noreply.github.com>
2026-08-31 20:01:11 +0900

    模範回答で上書き。

--- 0009_valid_palindrome.py
+++ 0009_valid_palindrome.py
@@ -1,8 +1,12 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+
         left, right = x, 0
         while left > right:
             amari = left % 10
-            left = x // 10
+            left = left // 10
             right = right * 10 + amari
-        if left = right or left // 10 =right
+
+        return left == right or left == right // 10
