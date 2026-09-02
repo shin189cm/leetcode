f1471a8466878e2f03b13ba7a3f65cff24b5ca30
shin189cm <156651244+shin189cm@users.noreply.github.com>
2026-09-02 22:37:46 +0900

    初見。何を問われているか全くわからなかった。

---
+++ 0703_kth_largest_element_in_a_stream.py
@@ -0,0 +1,18 @@
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        new KthLargest(k, nums)
+
+    def add(self, val: int) -> int:
+        """
+        ここでやることは？
+        __init__はどう呼び出されるの？
+        リストが完了するまで繰り返し処理だと思うが？
+        何を要求されているんだ？
+        """
+
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
