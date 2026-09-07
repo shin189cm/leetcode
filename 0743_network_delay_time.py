1c734299ce146e1c8784ec50f1a00191a733a621
shin189cm <156651244+shin189cm@users.noreply.github.com>
2026-09-07 22:50:42 +0900

    初見。何もできず。

---
+++ 0743_network_delay_time.py
@@ -0,0 +1,9 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        """
+        1-indexed
+        指定のkのnodeからシグナルを発したときに、すべてのnodeにシグナルが到達する最短時間は
+        dp[]
+        各ノードについて記録する。
+        各ノード
+        """
